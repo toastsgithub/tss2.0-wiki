b962888f2f9612070ee9744cbdea7b2e8a5e456d
czc14 <czc14@software.nju.edu.cn>
2016-07-07 11:39:58 +0800

    Login

--- tss_2/src/main/java/com.wiki/service/UserService.java
+++ tss_2/src/main/java/com.wiki/service/UserService.java
@@ -1,7 +1,10 @@
 package com.wiki.service;
 
+import com.wiki.domain.User;
+
 /**
  * Created by Administrator on 2016/7/7.
  */
-public class UserService {
+public interface UserService {
+    public User Login(String userName,String password);
 }
