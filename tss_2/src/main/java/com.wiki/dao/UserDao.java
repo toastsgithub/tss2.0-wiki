3afb2a2e072a3ad16293c7ac800fa380f56977cb
czc14 <czc14@software.nju.edu.cn>
2016-07-07 11:28:02 +0800

    Login Framework

---
+++ tss_2/src/main/java/com.wiki/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.wiki.dao;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ */
+public class UserDao {
+}
---
+++ tss_2/src/main/java/com.wiki/service.Impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.wiki.service.Impl;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ */
+public class UserServiceImpl {
+}
