a33195d5c6a7daf0d38921e9e8c571a3749d0bbb
dzm14 <dzm14@software.nju.edu.cn>
2016-07-09 23:17:34 +0800

    fix bugs

--- wiki/src/main/webapp/js/createClause.js
+++ wiki/src/main/webapp/js/createClause.js
@@ -9,18 +9,34 @@ function addClause(){
     var tags = document.getElementById("tag").value;
     var catagories = document.getElementById("type").value;
     var content = show_markdown();
-    alert("vjksadkjdas");
+
+    var data = {
+                   operation: "add",data:
+                   {
+                       time: "2015-01-01 11:11:11",
+                       username: "123",
+                       summary: "asdasdas",
+                       title: "c++",
+                       tags: "blablabla",
+                       categories:"c1/c2/c3",
+                       content: "markdown正文"
+                   }
+                   };
+    // alert("content:"+content);
+    // var data = {operations:'add',data:{time:time, username:'123', summary:summary,title: title, tags:tags,
+    //     categories:catagories,content:content}};
+    alert("data"+JSON.stringify(data));
     $.ajax({
         type:"post",
         url:"/content/add",
         contentType:'application/json;charset=utf-8',
-        data:{operations:'add',data:{time:time, username:'123', summary:summary,title: title, tags:tags,
-            categories:catagories,content:content}},
-        success:function(){
+        data:JSON.stringify(data),
+        success:function(data){
             alert("create successfully!");
             alert(data);
         },
         error:function(data){
+            alert("error");
             alert(JSON.stringify(data[0]));
         },
     })
