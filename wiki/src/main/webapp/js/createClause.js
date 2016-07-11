864d17f33fe63df40cea4b78826b0fc24ef93171
sh14 <2419335621@qq.com>
2016-07-11 10:46:14 +0800

    1. update js

--- wiki/src/main/webapp/js/createClause.js
+++ wiki/src/main/webapp/js/createClause.js
@@ -2,30 +2,30 @@
  * Created by zhengyunzhi on 16/7/9.
  */
 
-function addClause(){
+function addClause() {
     alert("judge");
-    var submit=true;
-    if(document.getElementById("name_input").value == ""){
-        document.getElementById("nameTip").innerHTML="请输入条目名称";
-        submit=false;
+    var submit = true;
+    if (document.getElementById("name_input").value == "") {
+        document.getElementById("nameTip").innerHTML = "请输入条目名称";
+        submit = false;
     }
-    if(document.getElementById("category_input").value == ""){
-        document.getElementById("typeTip").innerHTML="请选择类别";
-        submit=false;
+    if (document.getElementById("category_input").value == "") {
+        document.getElementById("typeTip").innerHTML = "请选择类别";
+        submit = false;
     }
-    if(document.getElementById("tags_input").value == ""){
-        document.getElementById("tagTip").innerHTML="请选择标签";
-        submit=false;
+    if (document.getElementById("tags_input").value == "") {
+        document.getElementById("tagTip").innerHTML = "请选择标签";
+        submit = false;
     }
-    if(submit==false){
+    if (submit == false) {
         return;
-    }else {
-        document.getElementById("nameTip").innerHTML="";
-        document.getElementById("typeTip").innerHTML="";
-        document.getElementById("tagTip").innerHTML="";
+    } else {
+        document.getElementById("nameTip").innerHTML = "";
+        document.getElementById("typeTip").innerHTML = "";
+        document.getElementById("tagTip").innerHTML = "";
     }
     alert("judge done");
-    
+
 
     var time = getTime();
     var title = document.getElementById("name_input").value;
@@ -38,43 +38,42 @@ function addClause(){
 
 
     var data = {
-                   operation: "add",data:
-                   {
-                       time: time,
-                       username: "123",
-                       summary: summary,
-                       title: title,
-                       tags: tags,
-                       categories: catagories,
-                       content: content
-                   }
-                   };
+        operation: "add", data: {
+            time: time,
+            username: "123",
+            summary: summary,
+            title: title,
+            tags: tags,
+            categories: catagories,
+            content: "21332432"
+        }
+    };
     // alert("content:"+content);
     alert("data:" + JSON.stringify(data));
     // var data = {operations:'add',data:{time:time, username:'123', summary:summary,title: title, tags:tags,
     //     categories:catagories,content:content}};
     $.ajax({
-        type:"post",
-        url:"/content/add",
-        contentType:'application/json;charset=utf-8',
-        data:JSON.stringify(data),
-        success:function(data){
+        type: "post",
+        url: "/content/add",
+        contentType: 'application/json;charset=utf-8',
+        data: JSON.stringify(data),
+        success: function (data) {
             alert("create successfully!");
         },
-        error:function(data){
+        error: function (data) {
             alert("error");
         },
     })
 }
 
-function getTime(){
+function getTime() {
     var date = new Date();
     var year = date.getFullYear();
-    var month = date.getMonth()+1;
+    var month = date.getMonth() + 1;
     var day = date.getDate();
     var hour = date.getHours();
     var minute = date.getMinutes();
     var second = date.getSeconds();
-    var time = year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second;
+    var time = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
     return time;
 }
