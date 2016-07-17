bbb1882ec22795310bab44c998d52460fe12d9af
sh14 <sh14@software.nju.edu.cn>
2016-07-17 11:37:11 +0800

    1. 延长默认session时间

--- wiki/src/main/java/tss2/wiki/control/UserController.java
+++ wiki/src/main/java/tss2/wiki/control/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
         LoginResult lresult = userService.Login(username, password);
         if (lresult != null && lresult.isIncluded()) {
             Cookie cookie = new Cookie("sessionID", new SessionServiceimpl().register(username).getSessionID());
-            cookie.setMaxAge(5 * 60);
+            cookie.setMaxAge(SessionServiceimpl.DEFAULT_SESSION_LIFETIME * 60);
             cookie.setPath("/");
             response.addCookie(cookie);
         }
