817abdf8385e329f3ab670a21d4d6ce5312420f7
whui14 <whui14@software.nju.edu.cn>
2016-07-25 17:38:55 +0800

    wo de xiu gai ji lu

--- wiki/src/main/webapp/js/change_record.js
+++ wiki/src/main/webapp/js/change_record.js
@@ -1,18 +1,27 @@
 /**
  * Created by wh on 2016/7/25.
  */
-function clearLogin(){
+function getRecord(){
     $.ajax({
-        url: '/user/logout',
-        type: 'get',
-        async: false,
-        success: function (data) {},
-        error: function (data) {
-            alert('error!');
+        url:'/modify/list',
+        type:'get',
+        success:function (data) {
+            if (data.error==0){
+                var ID = 0;
+                for (x in data.data){
+                    if(data.data[x].isread == 0){
+                        ID++;
+                    }
+                }
+                document.getElementById('message_num').innerHTML = ID;
+            }else{
+                alert('http返回的内容存在错误');
+            }
+        },
+        error:function(){
+            alert('error');
         }
     });
-    deleteCookie("login");
-    deleteCookie("username");
 
 
 }
