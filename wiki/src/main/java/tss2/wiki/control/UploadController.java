6d39c498954b9f393e46bc645ac56626f7771f29
sh14 <sh14@software.nju.edu.cn>
2016-07-25 14:57:24 +0800

    1. hahahahaha

--- wiki/src/main/java/tss2/wiki/control/UploadController.java
+++ wiki/src/main/java/tss2/wiki/control/UploadController.java
@@ -3,10 +3,14 @@ package tss2.wiki.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 /**
  * Created by coral on 16-7-25.
@@ -14,7 +18,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping(value = "/files")
 public class UploadController {
-    
+
+    @RequestMapping(value = "/image", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+    public @ResponseBody String getFile(@RequestParam(value = "image") File file) {
+        return file.getName();
+    }
+
     protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws ServletException {
         binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
     }
