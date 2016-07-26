aeedbde2f4a7fb90d4e7379115854691a424d124
sh14 <sh14@software.nju.edu.cn>
2016-07-26 11:59:55 +0800

    1. 改了一些代码 2. 加了一些注释

--- wiki/src/main/webapp/js/entry_content.js
+++ wiki/src/main/webapp/js/entry_content.js
@@ -22,7 +22,7 @@ function load_content(title){
             show_tips(1,'内容加载完毕');
             // alert(JSON.stringify(data));
             var markdown_data;
-            if (data.data.content) {
+            if (data.data) {
                 markdown = data.data.content;
                 var converter = new showdown.Converter(),
                     // text      = '#hello, markdown!\n\n|name|age|\n|--|--|\n|Toast|20|',
@@ -32,10 +32,11 @@ function load_content(title){
                 document.getElementById('access_times').innerHTML+=data.data.visits;
                 document.getElementById('last_update_time').innerHTML+=data.data.date;
             } else {
+                alert(1);
                 markdown_data = '当前不存在条目*' + title + '* 的详细内容。你可以[创建这个条目](http://localhost:8080/html/Entry_editor.html?title=' + title + ')。' +
                                 '或者你也[请求新增这个条目](/message/wiki?title=' + title + ')。 你也可以将这个条目创建为已存在条目的别名)。';
-
             }
+            alert(123);
 
             // alert(html);
                 //----------以下是内容的渲染-----------------
